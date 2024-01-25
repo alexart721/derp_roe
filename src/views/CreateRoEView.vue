2dcc12141bd5b7dfb31b0cace11c460f239ac6e4
Alex Teodorescu <ateodorescu721@gmail.com>
2024-01-25 00:39:00 -0500

    add fixed term started

--- src/views/CreateRoEView.vue
+++ src/views/CreateRoEView.vue
@@ -7,15 +7,17 @@
       <PvFieldset legend="Participants" class="mt-6">
         <p>This Challenge, with its terms defined in the following sections, is hereby issued by 
           <PvInputText type="text" size="small" placeholder="Challenger" v-model="challenger" />, henceforth known as the
-          <span class="font-italic font-semibold">Challenger</span>, to
+          <span class="font-italic font-semibold">Challenger</span>, at email address
+          <PvInputText type="text" size="small" placeholder="Challenger Email" v-model="challengerEmail" />, to
           <PvInputText type="text" size="small" placeholder="Challenged" v-model="challenged" />, henceforth known as the
-          <span class="font-italic font-semibold">Challenged</span>.
+          <span class="font-italic font-semibold">Challenged</span>, at email address
+          <PvInputText type="text" size="small" placeholder="Challenged Email" v-model="challengedEmail" />.
         </p>
       </PvFieldset>
       <PvFieldset legend="Fixed Terms" class="mt-6">
         <PvToolbar class="mb-4">
           <template #start>
-            <PvButton label="Add Fixed Term" icon="pi pi-plus" class="mr-2" outlined />
+            <PvButton label="Add Fixed Term" icon="pi pi-plus" class="mr-2" outlined @click="toggleFixedTermDialog" />
             <PvButton label="Delete" icon="pi pi-trash" severity="danger" outlined />
           </template>
         </PvToolbar>
@@ -46,12 +48,36 @@
         </PvDataTable>
       </PvFieldset>
       <PvToolbar class="mt-6 mb-6">
+        <template #start>
+          <PvButton label="Issue Challenge" icon="pi pi-chevron-circle-right" class="mr-2" />
+        </template>
         <template #end>
-          <PvButton label="Create RoE" icon="pi pi-save" class="mr-2" />
           <PvButton label="Start Over" icon="pi pi-ban" severity="danger" />
         </template>
       </PvToolbar>
     </div>
+
+    <PvDialog v-model:visible="addFixedTermDialog" :style="{width: '450px'}" header="Add Fixed Term" :modal="true" class="p-fluid">
+      <div class="field">
+        <label for="newFixedTerm">New Fixed Term</label>
+        <PvInputText id="newFixedTerm" v-model.trim="newFixedTerm.termDescription" required="true" autofocus :class="{'p-invalid': submitted && !newFixedTerm.termDescription}" />
+        <small class="p-error" v-if="submitted && !newFixedTerm.termDescription">Fixed Term must be defined.</small>
+      </div>
+      <div class="field">
+        <label for="newFixedTermChallengerValue">Challenger Value</label>
+        <PvInputText id="newFixedTermChallengerValue" v-model.trim="newFixedTerm.challengerValue" required="true" :class="{'p-invalid': submitted && !newFixedTerm.challengerValue}" />
+        <small class="p-error" v-if="submitted && !newFixedTerm.challengerValue">Challenger value must be defined.</small>
+      </div>
+      <div class="field">
+        <label for="newFixedTermChallengedValue">Challenged Value</label>
+        <PvInputText id="newFixedTermChallengedValue" v-model.trim="newFixedTerm.challengedValue" required="true" :class="{'p-invalid': submitted && !newFixedTerm.challengedValue}" />
+        <small class="p-error" v-if="submitted && !newFixedTerm.challengedValue">Challenged value must be defined.</small>
+      </div>
+      <template #footer>
+        <PvButton label="Add" icon="pi pi-check" text @click="addFixedTerm" />
+        <PvButton label="Cancel" icon="pi pi-times" text @click="toggleFixedTermDialog"/>
+      </template>
+    </PvDialog>
   </div>
 </template>
 
@@ -59,8 +85,29 @@
 import { ref } from 'vue';
 
 const challenger = ref<string>('');
+const challengerEmail = ref<string>('');
 const challenged = ref<string>('');
-const fixedTerms = ref([
+const challengedEmail = ref<string>('');
+const submitted = ref<boolean>(false);
+
+interface FixedTerm {
+  order: number | null,
+  termDescription: string | null,
+  challenger: string | null,
+  challengerValue: string | null,
+  challenged: string | null,
+  challengedValue: string | null,
+}
+
+const newFixedTerm = ref<FixedTerm>({
+  order: null,
+  termDescription: null,
+  challenger: challenger.value,
+  challengerValue: null,
+  challenged: challenged.value,
+  challengedValue: null,
+});
+const fixedTerms = ref<FixedTerm[]>([
   {
     order: 1,
     termDescription: 'Aircraft Selection',
@@ -71,6 +118,26 @@ const fixedTerms = ref([
   }
 ]);
 const selectedFixedTerms = ref([]);
+const addFixedTermDialog = ref<boolean>(false);
+const toggleFixedTermDialog = () => {
+  // Clear values?
+  addFixedTermDialog.value = !addFixedTermDialog.value;
+}
+const addFixedTerm = () => {
+  submitted.value = true;
+  console.log('here');
+  console.log(newFixedTerm.value);
+  
+  // add validator
+  if (newFixedTerm.value.termDescription && newFixedTerm.value.challengerValue && newFixedTerm.value.challengedValue) {
+    console.log('also here');
+    
+    newFixedTerm.value.order = fixedTerms.value.length + 1;
+    fixedTerms.value.push(newFixedTerm.value);
+    addFixedTermDialog.value = false;
+  }
+}
+
 const preDeterminationFateTerms = ref([
   {
     order: 1,
@@ -79,6 +146,7 @@ const preDeterminationFateTerms = ref([
   }
 ]);
 const selectedPreDeterminationFateTerms = ref([]);
+
 </script>
 
 <style scoped lang="scss">
